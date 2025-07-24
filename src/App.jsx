fe705bc9e0f995aadb40b40a80ad4c753f0e20d1
karthikeyeacoder <velivelakarthikeya@gmail.com>
2025-07-24 12:07:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="gradient-text">Hello Karthikeya</h1>
+    </div>
+  )
+}
+
+export default App
